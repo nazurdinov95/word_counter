4ba5d4cd71a9c0e5fe5bb0eae953044da717a6b6
amr <nazurdinov95>
2019-12-02 12:05:33 +0500

    deleted something

--- myscript.js
+++ myscript.js
@@ -5,23 +5,13 @@ const paragraphCount = document.getElementById('paragraphCount');
 const showWordCount = document.getElementById('showWordCount');
 
 countCharTextarea.addEventListener("keyup", function () {
-  const sentences = countCharTextarea.value.split(/[.|!|?]/g);
   countCharacters.innerHTML = countCharTextarea.value.length;
+  let sentences = countCharTextarea.value.split(/[.|!|?]/g);
   sentenceCount.innerHTML = sentences.length - 1;
+  let countWords = countCharTextarea.value.split(' ');
+  showWordCount.innerHTML = countWords.length;
 
-});
-
-function countWord(val) {
-  let wom = val.match(/\b[-?(\w+)?]+\b/gi);
-  return {
-    words: wom ? wom.length : 0
-  };
-}
-
-countCharTextarea.addEventListener('input', function () {
-  let wordCounter = countWord(this.value);
-  showWordCount.innerHTML = wordCounter.words;
-  if (wordCounter) {
+  if (countWords) {
     let paragraphs = countCharTextarea.value.replace(/\n$/gm, '').split(/\n/);
     paragraphCount.innerHTML = paragraphs.length;
   } else {
