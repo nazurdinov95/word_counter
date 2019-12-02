141476814b9ecfc2f5a9df1e72b65fc93e754411
amr <nazurdinov95>
2019-12-02 09:11:24 +0500

    change something and add paragraphCounter

--- myscript.js
+++ myscript.js
@@ -1,23 +1,30 @@
 const countCharTextarea = document.getElementById('countCharTextarea');
 const countCharacters = document.getElementById('countCharacters');
-//const characters = document.getElementById('characters');
+const sentenceCount = document.getElementById('sentenceCount');
+const paragraphCount = document.getElementById('paragraphCount');
+const showWordCount = document.getElementById('showWordCount');
 
-countCharTextarea.addEventListener("keyup",function() {
-  const characters = countCharTextarea.value.split(' ');
-  countCharacters.innerHTML = characters.length;
+countCharTextarea.addEventListener("keyup", function () {
+  const sentences = countCharTextarea.value.split(/[.|!|?]/g);
+  countCharacters.innerHTML = countCharTextarea.value.length;
+  sentenceCount.innerHTML = sentences.length - 1;
+
 });
 
 function countWord(val) {
-  let wom = val.match(/\S+/g);
+  let wom = val.match(/\b[-?(\w+)?]+\b/gi);
   return {
     words: wom ? wom.length : 0
   };
 }
 
-let showWordCount = document.getElementById('box');
 countCharTextarea.addEventListener('input', function () {
-  let v = countWord(this.value);
-  showWordCount.innerHTML = (
-      "<br>Words:" + v.words
-  );
-}, false);
+  let wordCounter = countWord(this.value);
+  showWordCount.innerHTML = wordCounter.words;
+  if (wordCounter) {
+    let paragraphs = countCharTextarea.value.replace(/\n$/gm, '').split(/\n/);
+    paragraphCount.innerHTML = paragraphs.length;
+  } else {
+    paragraphCount.innerHTML = 0;
+  }
+});
